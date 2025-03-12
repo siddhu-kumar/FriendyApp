abf07fb72d32a2f4e77e7eefdd0e282b5b4bdeb5
siddhu-kumar <youremail@example.com>
2025-03-12 09:54:41 +0530

    all

--- front-end/src/pages/userpage/register/Register.jsx
+++ front-end/src/pages/userpage/register/Register.jsx
@@ -1,52 +1,51 @@
 import React, { useContext, useState } from 'react'
 import style from './register.module.css'
 import { Link, useNavigate } from 'react-router-dom'
-import { createUser, validateUserData } from '../../../services/user-service'
-import { doLogin, getUserData, isLoggedIn } from '../../../auth'
+import { validateUserData } from '../../../services/user-service'
 import { UserContext } from '../../../context/userContext'
 import { validation } from '../../../auth/validation'
 import { emailValidate } from '../../../services/user-service'
 const Register = () => {
-  const {setAuth, setUserDetails, setReg} = useContext(UserContext)
+  const { setUserDetails } = useContext(UserContext)
   const navigate = useNavigate()
   const [userInput, setUserInput] = useState({
-    name:"",
-    email:"",
-    contact:"",
-    password:"",
+    name: "",
+    email: "",
+    contact: "",
+    password: "",
   })
-  const [data,setData] = useState(null)
+  const [data, setData] = useState(null)
   const handleChange = async (e) => {
-    const {name,value} = e.target;
-    setUserInput({...userInput,[name]:value})
+    const { name, value } = e.target;
+    setUserInput({ ...userInput, [name]: value })
   }
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     const isValidated = validation(userInput)
 
     if (isValidated.length === 0) {
       setUserDetails(userInput);
-      await validateUserData({contact:userInput.contact, email:userInput.email}).then(data => {
+      await validateUserData({ contact: userInput.contact, email: userInput.email }).then(data => {
         console.log(data)
       }).catch(err => {
-        if(err.response.data.flag){
-          console.log('p',err)
+        if (err.response.data.flag) {
+          console.log('p', err)
           setData(err.response.data.message)
-          return ;
+          return;
         }
       })
-      await emailValidate({email:userInput.email}).then(data => {
+      await emailValidate({ email: userInput.email }).then(data => {
         console.log(data);
       })
-      navigate('/otp-verify',{ state:{reg:true}})
+      navigate('/otp-verify', { state: { reg: true } })
     } else {
       isValidated.forEach(element => {
         alert(element)
       });
       return;
     }
-    
+
   }
 
   return (
@@ -55,16 +54,16 @@ const Register = () => {
         <span>FriendyApp</span>
       </div>
       <div className={style.userInput}>
-         <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="text">User Name</label>
-          <input type="text" 
+          <input type="text"
             placeholder='Username must be atleast 4 letter.'
             name='name'
             value={userInput.username}
             onChange={handleChange}
           />
           <label htmlFor="email">User Email</label>
-          <input type="text" 
+          <input type="text"
             placeholder='Someone@example.com'
             name='email'
             value={userInput.email}
@@ -72,8 +71,7 @@ const Register = () => {
             required
           />
           <label htmlFor="contact">User Contact</label>
-     
-          <input type="text" 
+          <input type="text"
             placeholder='+91 0123456789'
             name='contact'
             value={userInput.contact}
@@ -82,16 +80,16 @@ const Register = () => {
           />
           {data}
           <label htmlFor="password">User Password</label>
-          <input type="password" 
-            placeholder='Use strong passowrd.'
+          <input type="password"
+            placeholder='Use strong password.'
             name='password'
             value={userInput.password}
             onChange={handleChange}
             required
           />
           <button className={style.RegisterBtn} type='submit'>Register</button>
-          <span>Already have an account? <Link to="/">Log-In here</Link> !</span> 
-         </form>
+          <span>Already have an account? <Link to="/">Log-In here</Link> !</span>
+        </form>
       </div>
     </div>
   )
