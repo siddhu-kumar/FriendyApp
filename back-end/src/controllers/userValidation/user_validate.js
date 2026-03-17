2ffd8cf50ca4c8e57858e62c93c7641cefd3f119
siddhu-kumar <siddhu8252@gmail.com>
2026-03-17 18:48:47 +0530

    smtp mail port updated

--- back-end/src/controllers/userValidation/user_validate.js
+++ back-end/src/controllers/userValidation/user_validate.js
@@ -43,8 +43,8 @@ const nodemail_user_id = process.env.NODEMAIL_USER_ID;
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: nodemail_user_id,
     pass: pass_key,
